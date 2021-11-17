70e6825e4f29c9fe8d22faf230a62a09938af75b
nic0mp <nico.mp215@gmail.com>
2021-11-17 15:12:27 -0600

    first

---
+++ DOM_STUFF/js/main.js
@@ -0,0 +1,56 @@
+console.log(document.getElementsByTagName('h1')[0].innerHTML = 'Hola')
+
+let color_button = document.getElementById('color-button')
+console.log(color_button)
+
+function color_change(){
+    let header_text = document.getElementsByTagName('h1')[0].innerHTML
+    if(header_text== 'Hola'){
+        document.getElementsByTagName('h1')[0].className = 'color-change'
+    } else {
+        header_text = 'Something Else'
+    }
+}
+
+// Change text color via event listener in js
+color_button.addEventListener('click', color_change)
+
+// Adding New Button via JS
+let button = document.createElement('button')
+let button_display = document.createElement('h2')
+
+button.innerHTML = ' Here I am'
+document.body.append(button)
+
+button.addEventListener('click', function (){
+    button_display.innerHTML = 'More JS Info Here ...'
+    document.body.append(button_display)
+})
+
+// Grab copied text
+const source = document.querySelector('div.source')
+source.addEventListener('copy', (event ) =>{
+    
+    console.log(event)
+    const selection = document.getSelection();
+    event.clipboardData.setData('text/plain', selection.toString().toLowerCase());
+    event.preventDefault();
+})
+
+// Grabbing Form Data From a Submit event
+const form = document.querySelector('#testDataForm')
+console.log(form)
+
+// Add event listener for submit event
+form.addEventListener('submit', ( event ) => {
+    event.preventDefault();
+    let query_first = document.querySelector('#first-name');
+    let query_last = document.querySelector('#last-name')
+    console.log(query_first)
+    console.log(event)
+    let first_name = event.path[0][0].value;
+    let last_name = event.path[0][1].value;
+    
+    
+    console.log(`This came from the query selector: ${first_name}, ${last_name}`)
+} )
---
+++ RANGERS_API/js/main.js
@@ -0,0 +1,29 @@
+const getData = async () => {
+    let response = await axios.get(`https://my-json-server.typicode.com/CodingTemple/Power-Rangers-API-Json/rangers`)
+    console.log(response.data)
+    return response.data
+}
+// Create constants to hold DOM elements
+const DOM_Elements = {
+    rangers: '.ranger-list'
+}
+
+// Creation of the Ranger List HTML
+const create_list = (id, name, color, season) =>{ 
+    const html = `<a href ="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}"> ${name} 
+    </a>`;
+    // Access document to paste content
+    document.querySelector(DOM_Elements.rangers).insertAdjacentHTML('beforeend', html)
+}
+
+// Function to Load Data and display HTML 
+
+const loadData = async () => {
+    const rangers = await getData();
+
+    rangers.forEach( element => create_list(element.id, element.name, element.color[0], element.season));
+}
+
+const clearData = () =>{
+    document.querySelector(DOM_Elements.rangers).innerHTML = '';
+}
